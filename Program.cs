25627efeb37dc0543f4b938ddbf58f61f5abe585
Md. Jamil Istiaq <60819346+Jamil-Istiaq@users.noreply.github.com>
2020-08-26 19:29:01 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areaofractangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length, weigth, area;
+            Console.WriteLine("Enter the Value of Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the weigth: ");
+            weigth = Convert.ToDouble(Console.ReadLine());
+            area = length * weigth;
+            Console.WriteLine("The area of the rantangle is  " + area);
+        }
+    }
+}
